aa65e43f4f836e33e073afd2003f0a06fc91df64
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-14 20:23:23 +0300

    Implement Pages and routing as requested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,8 +20,32 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './pages/Root.js';
+import HomePage from './pages/Home.js';
+import EventsPage from './pages/Events.js';
+import EventDetailPage from './pages/EventDetail.js';
+import NewEventPage from './pages/NewEvent.js';
+import EditEventPage from './pages/EditEvent.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+    ]
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
