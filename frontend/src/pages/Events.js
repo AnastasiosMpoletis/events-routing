556588467a3ecd924f67983b1d5d03660e6a7b3f
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-15 12:56:55 +0300

    L-359, 360. Add Events and implement loading events from backend with route loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,25 +1,13 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from 'react-router-dom';
 
-const EVENTS = [
-    { id: 'e1', title: 'Event 1', },
-    { id: 'e2', title: 'Event 2', },
-    { id: 'e3', title: 'Event 3', },
-    { id: 'e4', title: 'Event 4', },
-];
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
+    const events = useLoaderData();
+
     return (
         <>
-            <h1>Events Page</h1>
-            <ul>
-                {EVENTS.map(event =>
-                    <li key={event.id}>
-                        <Link to={event.id}>
-                            {event.title}
-                        </Link>
-                    </li>
-                )}
-            </ul>
+            <EventsList events={events} />
         </>
     );
 }
